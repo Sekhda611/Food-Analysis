18b6904f19c51bc8f223de0d713d2a478f831ebc
Sonal A. Sekhda <122904485+Sekhda611@users.noreply.github.com>
2026-03-21 15:16:31 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import folium 
+from streamlit_folium import st_folium
+
+
+APP_TITLE = 'Food Access and Analysis'
+
+def display_map(df, state):
+    df = df[df['State'] == state]
+
+    m =folium.Map(location = [38,-96.5], zoom_start = 5, scrollWheelZoom = False)
+
+def main():
+    st.set_page_config(APP_TITLE)
+    st.title(APP_TITLE)
+
+    # Load Data
+    df_main = pd.read_csv('s_master_dataset_only_common_counties.csv')
+
+    #st.write(df_main.head())
+    #st.write(df_main.shape)
+    #st.write(df_main.isna().sum())
+
+    # Display filters and map
+    state = 'Alabama'
+    state_name = display_map(df_main, state)
+
+    # Display metrics
+
+
+
+if __name__ == '__main__':
+    main()
+
