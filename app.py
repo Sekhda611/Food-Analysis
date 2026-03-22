7db1103fd521fbe7ebaf7feb90cc0d2a3fd2c7e2
Sonal A. Sekhda <122904485+Sekhda611@users.noreply.github.com>
2026-03-22 00:08:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-#import folium 
-#from streamlit_folium import st_folium
+import folium 
+from streamlit_folium import st_folium
 
 
 APP_TITLE = 'Food Access and Analysis'
