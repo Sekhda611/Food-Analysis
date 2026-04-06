7ce0565c1efaec435fcd4d7ccc3f3af34faa0de3
Sonal A. Sekhda <122904485+Sekhda611@users.noreply.github.com>
2026-04-06 15:36:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import folium
 from streamlit_folium import st_folium
 
 
-APP_TITLE = 'Food Access and Analysis'
+APP_TITLE = 'Food Insecurity and Food Access Analysis'
 
 def display_map(df, state):
     df = df[df['State'] == state]
@@ -18,10 +18,59 @@ def main():
 
     # Load Data
     df_main = pd.read_csv('s_master_dataset_only_common_counties.csv')
+    st.sidebar.header('Filters')
+    level = st.sidebar.radio('Select Level', ['County', 'State'])
+    #select variable for ranking
+    metric = st.sidebar.selectbox(
+        "Select Metric for Top 10",
+        [
+        "Food_Insecurity_Rate",
+        "PovertyRate",
+        "MedianFamilyIncome",
+        "snap_participation_rate"
+        ]
+    )
+    # Select variables for scatter plot
+    x_var = st.sidebar.selectbox("X-axis", df.select_dtypes(include='number').columns)
+    y_var = st.sidebar.selectbox("Y-axis", df.select_dtypes(include='number').columns)
+
+    # Create a combined name column
+    df["County_State"] = df["County"] + ", " + df["State"]
+
+    # Aggregate if state level
+    if level == "State":
+        df_grouped = df.groupby("State").mean(numeric_only=True).reset_index()
+        df_grouped["Name"] = df_grouped["State"]
+    else:
+        df_grouped = df.copy()
+        df_grouped["Name"] = df_grouped["County_State"]
+
+    # Top 10
+    top10 = df_grouped.sort_values(metric, ascending=False).head(10)
+
+    st.subheader(f"Top 10 {level}s by {metric}")
+    # 📊 BAR CHART
+    fig_bar = px.bar(
+        top10,
+        x=metric,
+        y="Name",
+        orientation="h",
+        hover_data={
+            "Population": True if "Population" in df.columns else False,
+            "PovertyRate": True,
+            "MedianFamilyIncome": True
+        },
+        title=f"Top 10 {level}s by {metric}"
+    )
+
+    st.plotly_chart(fig_bar, use_container_width=True)
+    # DATA TABLE
+    st.subheader("Data Preview")
+    st.dataframe(top10)
 
     st.write(df_main.head())
     st.write(df_main.shape)
-    st.write(df_main.isna().sum())
+    
 
     #Display filters and map
     state = 'Alabama'
